eeb458ad17edfbb0546ea07538e65d4d2f9f438d
rcooper3307 <rcooper3307@bths.edu>
2018-11-20 21:24:17 -0500

    Initial commit

---
+++ src/com/company/CodeWordChecker.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class CodeWordChecker implements StringChecker {
+    private int min;
+    private int max;
+    private String str;
+    public CodeWordChecker(int min, int max, String str)
+    {
+        this.min = min;
+        this.max = max;
+        this.str = str;
+    }
+    public boolean isValid(String checkFor)
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ src/com/company/StringChecker.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public interface StringChecker {
+    boolean isValid(String checkFor)
+    {
+        if (str.indexOf(checkFor)) = 0) || (str.length() > min) || (str.length < max)
+            {
+                return false;
+            }
+        else
+        {
+            return true;
+        }
+    }
+}
